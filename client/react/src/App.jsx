776852f16b4b757b6763acc076e9022de0623119
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-05-03 19:35:24 +0300

    Set Up Router

--- client/react/src/App.jsx
+++ client/react/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+
+import { authContext } from './context/authContext';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showFilter, setShowFilter] = useState(false);
+
+  const onShowFilter = () => setShowFilter(!showFilter);
+  const ProvidedData = {
+    showFilter,
+    onShowFilter
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ marginTop: '50px', height: '100vh', backgroundColor: 'black' }}>
+      <authContext.Provider value={ProvidedData}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path='/' Component={Home} />
+            {/* <Route path='Login' Component={Home} /> */}
+            {/* <Route path='Profile' Component={Home} /> */}
+
+          </Routes>
+        </Router>
+      </authContext.Provider>
+    </div>
+  );
 }
 
 export default App

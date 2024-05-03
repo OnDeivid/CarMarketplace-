af35ad0a50488fc894edc357c996345babcd278d
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-05-03 19:38:28 +0300

    Added context provider for managing global state in React project.

---
+++ client/react/src/CONST.js
@@ -0,0 +1,23 @@
+import { IoHomeOutline } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { IoIosLogIn } from "react-icons/io";
+
+export const SideBarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: IoHomeOutline,
+    },
+    {
+        title: 'Login',
+        path: 'login',
+        icon: IoIosLogIn,
+    },
+    {
+        title: 'Profile',
+        path: 'profile',
+        icon: CgProfile,
+    },
+
+
+]

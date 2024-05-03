5fe3dbd982cf4f0749cd4ea637026eaa1d57b4b4
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-05-03 19:40:17 +0300

    Implement Filter component

---
+++ client/react/src/components/filter/Filter.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { authContext } from '../../context/authContext';
+import './Filter.css';
+
+export default function Filter() {
+    const { showFilter, onShowFilter } = useContext(authContext);
+    return (
+        <div className='container'>
+            <div className={showFilter ? 'filter-optionsActive' : 'filter-optionsHidden'}>
+                <ul>
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                    <li>5</li>
+                </ul>
+            </div>
+        </div>
+    );
+}

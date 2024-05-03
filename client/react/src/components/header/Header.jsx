8919abc4367988aa3de32a61e32c7958d2cb3d64
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-05-03 19:40:06 +0300

    Implement Header component

---
+++ client/react/src/components/header/Header.jsx
@@ -0,0 +1,40 @@
+import React, { useContext, useState } from 'react'
+import { Link } from 'react-router-dom';
+
+import * as FaIcons from "react-icons/fa";
+import { GrSearchAdvanced } from "react-icons/gr";
+import { MdHideSource } from "react-icons/md";
+
+import { SideBarData } from '../../CONST';
+
+import './Header.css'
+
+export default function Header() {
+    const [sideBar, setSideBar] = useState(false)
+    const showSideBar = () => setSideBar(prev => !prev)
+
+    return (
+        <>
+            <div className='navbar'>
+                <Link to='#' className='menu-bars'>
+                    {sideBar ? <MdHideSource onClick={showSideBar} /> : <FaIcons.FaBars onClick={showSideBar} />}
+                </Link>
+
+            </div>
+            <nav>
+                <ul className={sideBar ? 'nav-menu active' : 'nav-menu'}>
+                    {SideBarData.map((x, i) => {
+                        return (
+                            <li key={i} className={x.title}>
+                                <Link to={x.path} className='nav-text'>
+                                    <x.icon />
+                                    <span>{x.title}</span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        </>
+    )
+}

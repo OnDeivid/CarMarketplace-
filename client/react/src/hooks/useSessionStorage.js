3efa70ab258a4e2eee5e3cc1e5fc9c155ec8254c
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-05-25 18:16:28 +0300

    session storage hook

---
+++ client/react/src/hooks/useSessionStorage.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+function useSessionStorage(key, initialValue) {
+    const [state, setState] = useState(() => {
+        const persistedStateSerialized = sessionStorage.getItem(key)
+        if (persistedStateSerialized) {
+            const persistedState = JSON.parse(persistedStateSerialized)
+            return persistedState
+        }
+        return initialValue
+    })
+
+    const setSessionStorageState = (value) => {
+        setState(value)
+        sessionStorage.setItem(key, JSON.stringify(value))
+    }
+
+    return { state, setSessionStorageState }
+
+}

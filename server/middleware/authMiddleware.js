112e11d6536deb65aa41fe5b509b11e49fefa496
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-06-20 17:39:17 +0300

    create authMiddleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('../lib/jwt'); // Assuming jwt has a verify function
+
+exports.auth = async (req, res, next) => {
+    const token = req.cookies['token']
+    if (token) {
+        try {
+            const decodedToken = await jwt.verify(token, 'secret')
+            req.user = decodedToken
+            res.locals.userId = decodedToken._id
+            res.locals.email = decodedToken.email
+            next()
+
+        } catch (err) {
+            res.clearCookie('token')
+        }
+    }
+    else {
+        next()
+    }
+}

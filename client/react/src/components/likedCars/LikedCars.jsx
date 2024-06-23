af383d5a438df2e5720f6c3e953a9c99eb4f33e4
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-06-23 20:58:39 +0300

    remove hard coded data

--- client/react/src/components/likedCars/LikedCars.jsx
+++ client/react/src/components/likedCars/LikedCars.jsx
@@ -1,23 +1,13 @@
-import { useContext } from "react"
-import { authContext } from "../../context/authContext"
+// import { useContext } from "react"
+// import { authContext } from "../../context/authContext"
 
 import LikedCarsCard from "./LikedCarsCard"
 
 import './LikedCars.css'
-export default function LikedCars() {
+export default function LikedCars({showLiked}) {
 
-    let catalogItems = [
-        { id: 1, model: 'Item 1', mileage: '300', price: '$10', model: 'model of Item 1', imageUrl: 'https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' },
-        { id: 2, model: 'Item 2', mileage: '300', price: '$20', model: 'model of Item 2', imageUrl: 'https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' },
-        { id: 3, model: 'Item 3', mileage: '300', price: '$15', model: 'model of Item 3', imageUrl: 'https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' },
-        { id: 4, model: 'Item 4', mileage: '300', price: '$10', model: 'model of Item 1', imageUrl: 'https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' },
-        { id: 5, model: 'Item 5', mileage: '300', price: '$20', model: 'model of Item 2', imageUrl: 'https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' },
-        { id: 6, model: 'Item 6', mileage: '300', price: '$15', model: 'model of Item 3', imageUrl: 'https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' },
-        { id: 7, model: 'Item 7', mileage: '300', price: '$10', model: 'model of Item 1', imageUrl: 'https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' },
-        { id: 8, model: 'Item 8', mileage: '300', price: '$20', model: 'model of Item 2', imageUrl: 'https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' },
-        { id: 9, model: 'Item 9', mileage: '300', price: '$15', model: 'model of Item 3', imageUrl: 'https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' },
-    ]
-    const { showLiked } = useContext(authContext)
+   
+    // const { showLiked } = useContext(authContext)
 
     return (
         <div className='liked-container'>

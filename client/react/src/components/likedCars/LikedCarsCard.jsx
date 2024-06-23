6caee4fa8e1a8a5b55a9c45dc19a16ee85364cd3
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-06-23 20:58:15 +0300

    change prop name and some css

--- client/react/src/components/likedCars/LikedCarsCard.jsx
+++ client/react/src/components/likedCars/LikedCarsCard.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './LikedCarsCard.css'
 
-export default function LikedCarsCard({ item }) {
+export default function LikedCarsCard({ likedCars }) {
     return (
         <div className='likedPosts'>
             <div className="likedHolder">
                 <h3 style={{ color: 'white' }}>Audi RS6</h3>
-                <p style={{ color: 'white',marginTop:'-20px', marginLeft: '10px', fontSize: '10px' }}>uploaded on: 11.25.2023</p>
+                <p style={{ color: 'white', marginTop: '-20px', marginLeft: '10px', fontSize: '10px' }}>uploaded on: 11.25.2023</p>
                 <img src='https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' />
                 <div className='likedCarInfo'>
                     <p>Transm: Auto|</p>
@@ -18,7 +18,7 @@ export default function LikedCarsCard({ item }) {
             </div>
             <div className="likedHolder">
                 <h3 style={{ color: 'white' }}>Audi RS6</h3>
-                <p style={{ color: 'white',marginTop:'-20px', marginLeft: '10px', fontSize: '10px' }}>uploaded on: 11.25.2023</p>
+                <p style={{ color: 'white', marginTop: '-20px', marginLeft: '10px', fontSize: '10px' }}>uploaded on: 11.25.2023</p>
                 <img src='https://www.audiusa.com/content/dam/nemo/us/inside_audi/Innovation/1920x1080_MY24-RS6-Front-Parked.jpg' />
                 <div className='likedCarInfo'>
                     <p>Transm: Auto,</p>

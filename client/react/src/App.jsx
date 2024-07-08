1af09922587480f78473dae5a1625bd74178e9c5
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-07-08 20:41:13 +0300

    delete useless comments

--- client/react/src/App.jsx
+++ client/react/src/App.jsx
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
-import { useState } from 'react';
 
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 
 import { authContext } from './context/authContext';
+
 import { GET } from './requester';
 
 import Login from './components/login/Login';
 import Register from './components/register/Register';
 import Profile from './components/profile/Profile';
-// import LikedCars from './components/likedCars/LikedCars';
 import useSessionStorage from './hooks/useSessionStorage'
 import Logout from './components/logout/Logout';
 import Create from './components/create/Create';
@@ -18,24 +17,19 @@ import PrivateRoute from './privateRoute/PrivateRouter';
 import PublicRoute from './publicRoute/PublicRoute';
 
 function App() {
-  // const [showFilter, setShowFilter] = useState(false);
-  // const [showLiked, setShowLiked] = useState(false);
-  // const onShowFilter = () => setShowFilter(!showFilter);
-  // const onShowLiked = () => setShowLiked(!showLiked);
+  console.log('APP')
   const [auth, setAuth] = useSessionStorage('auth', '');
 
   const ProvidedData = {
     auth, setAuth,
-    // showFilter,
-    // onShowFilter,
-    // showLiked,
-    // onShowLiked
   };
 
   function onLogout() {
+
     setAuth('')
     GET('logout')
     sessionStorage.removeItem('auth')
+
   }
 
   return (
@@ -51,7 +45,6 @@ function App() {
             <Route path='/register' element={<PublicRoute><Register /></PublicRoute>} />
             <Route path='/' element={<Home />} />
           </Routes>
-          {/* {showLiked && <LikedCars />} */}
         </Router>
       </authContext.Provider>
     </div >

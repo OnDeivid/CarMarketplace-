101b1072210c0e72907c36e9ebef36bb708cafd2
t1n0-3 <141053918+OnDeivid@users.noreply.github.com>
2024-07-26 20:50:13 +0300

    update DB endpoint

--- server/src/config/connectDB.js
+++ server/src/config/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = () => {
-    mongoose.connect('mongodb://127.0.1:27017/CarMarketPlace')
+    mongoose.connect('mongodb://localhost:27017/CarMarketPlace')
         .then(() => console.log('db connected'))
         .catch((e) => console.log(e))
 }
